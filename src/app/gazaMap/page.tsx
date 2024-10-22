43ef8ebdc29434791e85f94fc8522c46b35c7457
DoniaBatool <donia1510aptech@gmail.com>
2024-10-22 17:43:18 +0500

    padding improved

--- src/app/gazaMap/page.tsx
+++ src/app/gazaMap/page.tsx
@@ -7,27 +7,27 @@ const GazaMaps = () => {
       <h6 className={styles.divh6}>Israel-Gaza War</h6>
       
       <h1 className={styles.h1}>Gaza-Nowhere Safe To Go!!</h1>
-      <h4 className={styles.h4}>
+      <p className={styles.h4}>
         After ordering 1.1 million people to leave the Northern Gaza Strip, 
-        the Israeli Army published <br/>a map online that divides the besieged enclave into more than 600 numbered zones with instructions <br/>
+        the Israeli Army published a map online that divides the besieged enclave into more than 600 numbered zones with instructions
         to evacuate part of the south.
-      </h4>
+      </p>
       <Image src="/gaza/map1.jpg" alt="map1" width={500} height={450} className={styles.image} />
 
       <h6 className={styles.divh6}>Israel-Palestine Conflict</h6>
       <h1 className={styles.h1}>The Besieged Gaza Strip</h1>
-      <h4 className={styles.h4}>
-        The Palestinian enclave — home to 2.3 million people — has been under an Israeli air,<br/> land, and sea blockade since 2007.
-      </h4>
+      <p className={styles.h4}>
+        The Palestinian enclave — home to 2.3 million people — has been under an Israeli air,land and sea blockade since 2007.
+      </p>
       <Image src="/gaza/map2.jpg" alt="map2" width={500} height={450} className={styles.image} />
 
       <h6 className={styles.divh6}>Israel-Palestine Conflict</h6>
       <h1 className={styles.h1}>How The Hamas Unfolded</h1>
-      <h4 className={styles.h4}>
-        On the morning of October 7, Saturday, Hamas fired thousands of rockets and sent fighters <br/>
+      <p className={styles.h4}>
+        On the morning of October 7, Saturday, Hamas fired thousands of rockets and sent fighters
         into Israel in a surprise attack. Israel responded with a deadly bombardment of the
-        <br/> blockaded coastal enclave and declared a state of war.
-      </h4>
+        blockaded coastal enclave and declared a state of war.
+      </p>
       <Image src="/gaza/map3.jpg" alt="map3" width={500} height={450} className={styles.image} />
     </div>
   );
